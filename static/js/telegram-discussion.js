6b7f9ea810f7aef5f313f2792bff5d45ccf14830
Eric Vlaskin <eric@vlaskin.org>
2026-04-26 01:55:33 +0300

    Update Telegram comments script

--- static/js/telegram-discussion.js
+++ static/js/telegram-discussion.js
@@ -1,15 +1,44 @@
-const script = document.createElement('script')
-script.async = true
-script.src = 'https://telegram.org/js/telegram-widget.js?22'
-script.dataset.telegramDiscussion = 'ITDOXY'
-script.dataset.commentsLimit = '5'
-script.dataset.colorful = '1'
-script.dataset.color = '343638'
-script.dataset.darkColor = 'FFFFFF'
+const commentsContainer = document.getElementsByClassName('telegram-comments')[0]
 
-if (document.querySelector('html').dataset.theme === 'dark') {
-    script.dataset.dark = '1'
+const TG_CONFIG = {
+    channel: 'ITDOXY',
+    commentsLimit: 5,
+    colorful: 1,
+    color: '343638',
+    darkColor: 'FFFFFF',
 }
 
-const comments = document.getElementsByClassName('telegram-comments')[0]
-comments.appendChild(script)
+function initTelegramWidget() {
+    const isDark = document.querySelector('html').dataset.theme === 'dark'
+
+    const script = document.createElement('script')
+    script.async = true
+    script.src = 'https://telegram.org/js/telegram-widget.js?22'
+    script.dataset.telegramDiscussion = TG_CONFIG.channel
+    script.dataset.commentsLimit = String(TG_CONFIG.commentsLimit)
+    script.dataset.colorful = String(TG_CONFIG.colorful)
+    script.dataset.color = TG_CONFIG.color
+    script.dataset.darkColor = TG_CONFIG.darkColor
+    if (isDark) {
+        script.dataset.dark = '1'
+    }
+    commentsContainer.appendChild(script)
+}
+
+initTelegramWidget()
+
+function updateTelegramIframe(dark) {
+    const iframe = commentsContainer.querySelector('iframe')
+    if (iframe) {
+        let src = iframe.src
+            .replace(/&dark=[^&]*/, '')
+        if (dark) {
+            src += `&dark=1`
+        }
+        iframe.src = src
+    }
+}
+
+window.addEventListener('theme-changed', (e) => {
+    updateTelegramIframe(e.detail.theme === 'dark')
+})
